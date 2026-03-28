99f231db58eb23eead6a829f312a654b8c126e21
kotayam <ykomako4.9@gmail.com>
2026-03-28 14:06:04 -0400

    add message passing

--- shim/shim.c
+++ shim/shim.c
@@ -1,6 +1,21 @@
 #include <stddef.h>
+#include <stdint.h>
 
-// implement strlen
+#define MESSAGE_BUFFER_SIZE 4096
+
+static char message_buffer[MESSAGE_BUFFER_SIZE];
+
+// copy src to dest
+void *memcpy(void *dest, const void *src, size_t n) {
+  char *d = dest;
+  const char *s = src;
+  while (n--) {
+    *d++ = *s++;
+  }
+  return dest;
+}
+
+// get length of string
 size_t strlen(const char *s) {
   size_t len = 0;
   while (s[len] != '\0') {
@@ -9,21 +24,21 @@ size_t strlen(const char *s) {
   return len;
 }
 
-// hardware port interaction
-static inline void outb(unsigned short port, unsigned char value) {
-  asm volatile("outb %0, %1" : : "a"(value), "Nd"(port));
+// 32-bit hardware port interaction
+static inline void outl(uint16_t port, uint32_t value) {
+  asm volatile("outl %0, %1" : : "a"(value), "Nd"(port));
 }
 
 // intercept write
 long write(int fd, const void *buf, size_t count) {
-  (void)fd; // We are ignoring file descriptors (like stdout = 1) for now
+  // copy user data in to message buffer
+  memcpy(message_buffer, buf, count);
 
-  const char *char_buf = (const char *)buf;
+  // tell host the address of buffer via port 0x10
+  outl(0x10, (uint32_t)(uintptr_t)message_buffer);
 
-  // Send each character to port 0xE9 for the Host Loader to catch
-  for (size_t i = 0; i < count; i++) {
-    outb(0xE9, char_buf[i]);
-  }
+  // tell host the length of the data via port 0x11
+  outl(0x11, count);
 
   return count; // Return the number of bytes successfully written
 }
