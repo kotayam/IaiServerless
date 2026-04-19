e5461727fc51d5e674ca86e11449cd27e656f391
kotayam <ykomako4.9@gmail.com>
2026-04-19 10:57:02 -0400

    add test for brk

---
+++ samples/test_brk.c
@@ -0,0 +1,52 @@
+long write(int fd, const void *buf, unsigned long count);
+void *sbrk(long increment);
+int brk(void *addr);
+
+void print_str(const char *s) {
+    unsigned long len = 0;
+    while (s[len]) len++;
+    write(1, s, len);
+}
+
+void print_hex(unsigned long val) {
+    char buf[19] = "0x";
+    char *p = buf + 2;
+    for (int i = 60; i >= 0; i -= 4) {
+        int digit = (val >> i) & 0xF;
+        *p++ = digit < 10 ? '0' + digit : 'a' + digit - 10;
+    }
+    *p++ = '\n';
+    write(1, buf, p - buf);
+}
+
+int main() {
+    print_str("Testing brk/sbrk...\n");
+    
+    // Get initial break
+    void *initial = sbrk(0);
+    print_str("Initial brk: ");
+    print_hex((unsigned long)initial);
+    
+    // Allocate 1KB
+    void *p1 = sbrk(1024);
+    print_str("After sbrk(1024): ");
+    print_hex((unsigned long)p1);
+    
+    // Get current break
+    void *current = sbrk(0);
+    print_str("Current brk: ");
+    print_hex((unsigned long)current);
+    
+    // Try to set brk below _end (should fail)
+    int ret = brk((void *)0x1000);
+    print_str("brk(0x1000) returned: ");
+    print_hex(ret);
+    
+    // Try to set brk beyond memory (should fail)
+    ret = brk((void *)0x10000000);
+    print_str("brk(0x10000000) returned: ");
+    print_hex(ret);
+    
+    print_str("Test complete!\n");
+    return 0;
+}
