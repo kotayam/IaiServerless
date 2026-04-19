c8e19c2b0a7a9f86a8b5b6fcd56003dd824a03ef
kotayam <ykomako4.9@gmail.com>
2026-04-19 11:27:11 -0400

    take memory as arg

--- scripts/run_sample.sh
+++ scripts/run_sample.sh
@@ -4,11 +4,12 @@
 
 # Check if at least one argument is provided
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 [-v] <sample_binary.elf>"
+    echo "Usage: $0 [-v] [-m memory_in_MB] <sample_binary.elf>"
     exit 1
 fi
 
 VERBOSE_FLAG=""
+MEMORY_FLAG=""
 BINARY=""
 
 # Parse arguments
@@ -17,6 +18,10 @@ while [[ $# -gt 0 ]]; do
         -v)
             VERBOSE_FLAG="-v"
             shift
+            ;;
+        -m)
+            MEMORY_FLAG="-m $2"
+            shift 2
             ;;
         *)
             BINARY=$1
@@ -36,4 +41,4 @@ else
 fi
 
 echo "--- Executing $BINARY in IaiServerless ---"
-./host/host_loader $VERBOSE_FLAG "$BINARY"
+./host/host_loader $VERBOSE_FLAG $MEMORY_FLAG "$BINARY"
