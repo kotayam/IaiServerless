ff49dce80b1af04f83256b8c3edccaa57ccd487d
kotayam <ykomako4.9@gmail.com>
2026-04-19 21:13:19 -0400

    allow string slicing

--- external/python-port/mpconfigport.h
+++ external/python-port/mpconfigport.h
@@ -34,6 +34,9 @@ typedef long ssize_t;
 // Enable % string formatting
 #define MICROPY_PY_BUILTINS_STR_OP_MODULO (1)
 
+// Enable string/list slicing
+#define MICROPY_PY_BUILTINS_SLICE         (1)
+
 // Enable input() builtin backed by mp_hal_stdin_rx_chr
 #define MICROPY_PY_BUILTINS_INPUT         (1)
 struct _vstr_t;
